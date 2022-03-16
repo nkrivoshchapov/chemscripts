Fixed signs of orbitals

blender/blmol.py
@@ -289,14 +289,14 @@ class Bond:
         rot_matrix_x = rot_matrix_x/np.linalg.norm(rot_matrix_x)
         rot_matrix = [rot_matrix_x, rot_matrix_y, rot_matrix_z]
 
-        # ov = bpy.context.copy()
-        # ov['area'] = [a for a in bpy.context.screen.areas if a.type == "VIEW_3D"][0]
-        # bpy.ops.transform.rotate(ov, value=rot_angle, orient_axis='Z',
-        #                          orient_matrix=rot_matrix,
-        #                          constraint_axis=(False, False, True))
-        bpy.ops.transform.rotate(value=rot_angle, orient_axis='Z',
+        ov = bpy.context.copy()
+        ov['area'] = [a for a in bpy.context.screen.areas if a.type == "VIEW_3D"][0]
+        bpy.ops.transform.rotate(ov, value=rot_angle, orient_axis='Z',
                                  orient_matrix=rot_matrix,
                                  constraint_axis=(False, False, True))
+        # bpy.ops.transform.rotate(value=rot_angle, orient_axis='Z',
+        #                          orient_matrix=rot_matrix,
+        #                          constraint_axis=(False, False, True))
         bpy.ops.object.shade_smooth()
 
         if edge_split:
@@ -351,7 +351,7 @@ class Bond:
             rot_axis = np.array((1, 0, 0))
         angle = -np.arccos(np.dot(cyl_axis, bond_axis))
 
-        print("%d & %d -- %s & %s" % (self.atom1.id_num, self.atom2.id_num, repr(self.atom1.small), repr(self.atom2.small)))
+        # print("%d & %d -- %s & %s" % (self.atom1.id_num, self.atom2.id_num, repr(self.atom1.small), repr(self.atom2.small)))
         if self.atom1.small or self.atom2.small:
             print("Changed radius")
             radius = 0.05

nbo/isosurfaces.py
@@ -146,10 +146,10 @@ def get_nbo_direction(surfnames):
     center = np.zeros(3)
     for v in vertices:
         center += v
-    center /= len(center)
+    center /= len(vertices)
 
     for item in vertices:
         item -= center
     pca = sklearn.decomposition.PCA(3)
     pca.fit(vertices)
-    return pca.components_[0]
+    return pca.components_[0], center