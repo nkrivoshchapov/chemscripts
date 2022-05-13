361677b54e4ab998566594058fb72e084ca56360
Nikolai Krivoshchapov <knvvv@yandex.ru>
2022-05-13 17:28:57 +0300

    GoodVibes calc made optional

--- energyutils.py
+++ energyutils.py
@@ -11,18 +11,16 @@ class _Names(object):
     QH_ENERGY_COL = "QH free energy, a.u."
 
 
-def get_columns(get_scf: bool):
+def get_columns(get_scf: bool, get_free: bool):
+    res = [_Names.LOGNAME_COL]
     if get_scf:
-        return [_Names.LOGNAME_COL,
-                _Names.SCF_ENERGY_COL,
-                _Names.C0_COL,
+        res += [_Names.SCF_ENERGY_COL]
+    if get_free:
+        res += [_Names.C0_COL,
                 _Names.QH_ENERGY_COL]
-    else:
-        return [_Names.LOGNAME_COL,
-                _Names.C0_COL,
-                _Names.QH_ENERGY_COL]
+    return res
 
-def get_energy_sheet(filemask=None, excelsheet=None, get_scf=False):
+def get_energy_sheet(filemask=None, excelsheet=None, get_scf=False, get_free=True):
     assert filemask is not None or excelsheet is not None
     assert not (filemask is not None and excelsheet is not None)
     if filemask is not None:
@@ -30,15 +28,18 @@ def get_energy_sheet(filemask=None, excelsheet=None, get_scf=False):
 
         excelsheet = ExcelSheet()
         excelsheet.add_block(blockname=_Names.ENERGY_BLOCK,
-                             cols=get_columns(get_scf))
+                             cols=get_columns(get_scf, get_free))
 
         for file in glob.glob(filemask):
-            newitem = {_Names.LOGNAME_COL: file, _Names.C0_COL: 1.0}
+            newitem = {_Names.LOGNAME_COL: file}
+            if get_free:
+                newitem[_Names.C0_COL] = 1.0
             excelsheet.add_row(blockname=_Names.ENERGY_BLOCK, data=newitem)
 
     energy_block = excelsheet.block(_Names.ENERGY_BLOCK)
     for item in energy_block['data']:
-        item[_Names.QH_ENERGY_COL] = get_goodvibes_g(item[_Names.LOGNAME_COL], conc=item[_Names.C0_COL])
+        if _Names.QH_ENERGY_COL in energy_block['keys']:
+            item[_Names.QH_ENERGY_COL] = get_goodvibes_g(item[_Names.LOGNAME_COL], conc=item[_Names.C0_COL])
         if _Names.SCF_ENERGY_COL in energy_block['keys']:
             item[_Names.SCF_ENERGY_COL] = get_gaussian_scfener(item[_Names.LOGNAME_COL])
     return excelsheet
@@ -49,12 +50,13 @@ def energies_to_excel(filemask, excelname): # This is useless
     mysheet.save_xlsx(excelname)
 
 
-def prepare_sheet(filemask, get_scf=False):
+def prepare_sheet(filemask, get_scf=False, get_free=True):
     excelsheet = ExcelSheet()
     excelsheet.add_block(blockname=_Names.ENERGY_BLOCK,
                          cols=get_columns(get_scf))
     for file in glob.glob(filemask):
-        excelsheet.add_row(blockname=_Names.ENERGY_BLOCK, data={
-                                _Names.LOGNAME_COL: file,
-                                _Names.C0_COL: 1.0})
+        newitem = {_Names.LOGNAME_COL: file}
+        if get_free:
+            newitem[_Names.C0_COL] = 1.0
+        excelsheet.add_row(blockname=_Names.ENERGY_BLOCK, data=newitem)
     return excelsheet
